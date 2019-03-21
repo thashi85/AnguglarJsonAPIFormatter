bf1877f8731eea04e0b973d6c47f8528b313fb94
thashi.ucsc@gmail.com <thashi.ucsc@gmail.com>
2019-03-21 16:04:35 +0530

    library added

--- projects/json-apiformat/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of json-apiformat
- */
-
-export * from './lib/json-apiformat.service';
-export * from './lib/json-apiformat.component';
-export * from './lib/json-apiformat.module';
